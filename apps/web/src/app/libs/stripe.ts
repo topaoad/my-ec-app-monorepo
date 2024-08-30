9fb9ba40fae848b19583747ae971d06ac34442e1
toproad <tattun0817@gmail.com>
2024-08-30 15:15:25 +0900

    STRIPE_SECRET_API_KEYの環境変数調査

--- apps/web/src/app/libs/stripe.ts
+++ apps/web/src/app/libs/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_API_KEY) {
+  console.error("STRIPE_SECRET_API_KEY is not set");
   throw new Error("STRIPE_SECRET_API_KEY is required");
 }
 
