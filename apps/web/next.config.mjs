37511019e9273cb169538423ae7b63bbb9a0553e
toproad <tattun0817@gmail.com>
2024-08-30 13:42:20 +0900

    envの設定追加

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.microcms-assets.io"],
   },
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
 };
 
 export default nextConfig;
