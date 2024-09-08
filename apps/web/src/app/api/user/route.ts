e38e44d40f5001e96ab4a3f3a0a2ce64b9a1c622
toproad <tattun0817@gmail.com>
2024-09-09 08:11:56 +0900

    prismaのformコンポーネントインストールとルートハンドラー作成

--- apps/web/src/app/api/user/route.ts
+++ apps/web/src/app/api/user/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
 import { getUsers } from "@/app/libs/crud/getUsers";
 import { prisma } from "@/app/libs/prisma";
+import { NextResponse } from "next/server";
 
 // ユーザー情報とそのプロファイル情報を取得する
 export const GET = async (req: Request) => {
@@ -11,6 +11,25 @@ export const GET = async (req: Request) => {
 
     console.log(users);
     return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  } finally {
+    //$disconnectは推奨されているので実行する
+    await prisma.$disconnect();
+  }
+};
+
+// ユーザー情報とそのプロファイル情報を更新する
+export const PUT = async (req: Request) => {
+  try {
+    const { id, name } = await req.json();
+    // ユーザー情報を更新
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: { name },
+    });
+
+    return NextResponse.json({ updatedUser }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error" }, { status: 500 });
   } finally {
