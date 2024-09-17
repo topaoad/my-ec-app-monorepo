145227c38c25e11e3fd0c5bcf2f0fe8decbcda95
toproad <tattun0817@gmail.com>
2024-09-17 19:05:31 +0900

    Tailwindの導入と、それに伴うThemedTextの型定義の変更

--- apps/native-app/components/ThemedText.tsx
+++ apps/native-app/components/ThemedText.tsx
@@ -6,6 +6,7 @@ export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
   type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  className?: string;
 };
 
 export function ThemedText({
